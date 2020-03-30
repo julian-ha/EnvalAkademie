06c594ba8c4825d92ed02acf4369d8253099a530
envalde <kohoutdanie@gmail.com>
2020-03-30 10:42:35 +0200

    weiter aufräumen

--- client/src/components/button/button.js
+++ client/src/components/button/button.js
@@ -1,16 +1,16 @@
-import React, { Component} from 'react';
-import './../App.css';
-
+import React, { Component } from "react";
 
 class Button extends Component {
-
-    render (){
-        return (
-        <button id={ this.props.id } class={'button ' + this.props.class } name={ this.props.name }>
-            { this.props.innerhtml }
-        </button>
-    )
-    }
-    
+  render() {
+    return (
+      <button
+        id={this.props.id}
+        className={"button " + this.props.class}
+        name={this.props.name}
+      >
+        {this.props.innerhtml}
+      </button>
+    );
+  }
 }
 export default Button;
--- client/src/components/input/input.js
+++ client/src/components/input/input.js
@@ -1,44 +1,39 @@
-import React, { Component} from 'react';
-import './../App.css';
-
+import React, { Component } from "react";
 
 class Input extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: "Test"
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "Test"
+    };
+  }
 
-
-    render () {
-        return (
-            
-                <input id={this.props.id} name={this.props.name} class= {'input ' + this.props.class} type={this.props.type}></input>
-           
-        )
-    }
+  render() {
+    return (
+      <input
+        id={this.props.id}
+        name={this.props.name}
+        class={"input " + this.props.class}
+        type={this.props.type}
+      ></input>
+    );
+  }
 }
 
 class Inputfield extends Component {
-
-
-
-    renderInput(id, name, classe, type){
-        return <Input id={ id } name={ name } class={ classe } type= { type } />
-    }
-    render () {
-        return (
-           
-                <div class="field">
-                    <label class="label">ASDFasdfasdf</label>
-                    <div>
-                        {this.renderInput(this.props.id, "testname", "testclass", "text")}   
-                    </div>
-                </div>  
-            
-        )
-    }
+  renderInput(id, name, classe, type) {
+    return <Input id={id} name={name} class={classe} type={type} />;
+  }
+  render() {
+    return (
+      <div class="field">
+        <label class="label">ASDFasdfasdf</label>
+        <div>
+          {this.renderInput(this.props.id, "testname", "testclass", "text")}
+        </div>
+      </div>
+    );
+  }
 }
-    
-export default Inputfield ;
+
+export default Inputfield;

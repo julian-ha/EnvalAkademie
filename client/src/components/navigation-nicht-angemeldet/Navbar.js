84066e5668755b183714c3273c73010b19de74b5
envalde <kohoutdanie@gmail.com>
2020-03-30 12:34:23 +0200

    nav

--- client/src/components/navigation-nicht-angemeldet/Navbar.js
+++ client/src/components/navigation-nicht-angemeldet/Navbar.js
@@ -18,22 +18,15 @@ class Navigation extends Component {
           <nav
             className="navbar"
             aria-label="main navigation"
-            style={{
-              borderBottom: "solid 1px #dddddd"
-            }}
+            // style={{
+            //   borderBottom: "solid 1px #dddddd"
+            // }}
           >
             <div className="navbar-brand">
-              <a className="navbar-item">
+              <a href="#" className="navbar-item">
                 <img
-                  style={{
-                    borderTopLeftRadius: "50%",
-                    borderTopRightRadius: "50%",
-                    borderBottomLeftRadius: "50%",
-                    borderBottomRightRadius: "50%",
-                    marginRight: 15
-                  }}
-                  src="https://media-exp2.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAU3AAAAJGE1MzYxNzYzLTE1NTUtNDEyYi04MzRjLTgzZjNkOGU0MGIzNg.jpg"
-                  width="30px"
+                  src="https://enval.de/images/logo_slogan.svg"
+                  width="100px"
                   alt=""
                 />
                 <span>Akademie</span>
@@ -50,56 +43,27 @@ class Navigation extends Component {
               }
             >
               <div className="navbar-start">
-                <a className="navbar-item">
-                  <span
-                    className="icon has-text-primary"
-                    style={{ marginRight: 5 }}
-                  >
-                    <i className="fas fa-code"></i>
-                  </span>
+                <a className="navbar-item" href="#">
                   Code Blog
                 </a>
-                <a className="navbar-item">
-                  <span className="icon" style={{ marginRight: 5 }}>
-                    <i className="fab fa-lg fa-medium"></i>
-                  </span>
+                <a className="navbar-item" href="#">
                   Medium
                 </a>
                 <div className="navbar-item has-dropdown is-hoverable">
-                  <a className="navbar-link">Projects</a>
+                  <a className="navbar-link" href="#">
+                    Projects
+                  </a>
                   <div className="navbar-dropdown">
-                    <a className="navbar-item">Overview</a>
                     <hr className="navbar-divider" />
-                    <a className="navbar-item">This Site</a>
-                    <a className="navbar-item">Angular The React Way</a>
+                    <a className="navbar-item" href="#">
+                      Angular The React Way
+                    </a>
                   </div>
                 </div>
               </div>
               <div className="navbar-end">
-                <a
-                  className="navbar-item"
-                  href="https://github.com/aaronklaser"
-                >
-                  <span className="icon">
-                    <i className="fab fa-lg fa-github"></i>
-                  </span>
-                </a>
-                <a className="navbar-item" href="https://twitter.com/awklaser">
-                  <span
-                    className="icon has-text-info"
-                    style={{ color: "#0084FF" }}
-                  >
-                    <i className="fab fa-lg fa-twitter"></i>
-                  </span>
-                </a>
-                <a className="navbar-item" href="http://resume.aaronklaser.com">
+                <a className="navbar-item button is-primary" href="#">
                   Resume
-                  <span
-                    className="icon"
-                    style={{ color: "#0077B5", marginLeft: 5 }}
-                  >
-                    <i className="fab fa-lg fa-linkedin"></i>
-                  </span>
                 </a>
               </div>
             </div>
